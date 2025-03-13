739d7509e5e60701315ff45a04918e384ac81498
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-03-13 17:04:51 -0300

    ref controller

--- src/main/java/br/com/example/bank/controller/HomeController.java
+++ src/main/java/br/com/example/bank/controller/HomeController.java
@@ -1,5 +1,9 @@
 package br.com.example.bank.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/")
 public class HomeController {
